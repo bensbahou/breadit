26f6c8a733165166f872c76cb2246df4e261c8ce
bensbahou <younesbensbahou@gmail.com>
2023-10-26 19:06:30 +0100

    sing in up modal

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,8 +12,10 @@ export const metadata = {
 const inter = Inter({ subsets: ["latin"] });
 export default function RootLayout({
   children,
+  authModal,
 }: {
   children: React.ReactNode;
+  authModal: React.ReactNode;
 }) {
   return (
     <html
@@ -24,8 +26,9 @@ export default function RootLayout({
       )}
     >
       <body className="min-h-screen pt-12 bg-slate-50 antialiased">
+        {/* @ts-expect-error Server Component */}
         <Navbar />
-
+        {authModal}
         <div className="container max-w-7xl mx-auto h-full pt-12">
           {children}
         </div>
